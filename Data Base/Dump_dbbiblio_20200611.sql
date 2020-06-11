d98f40ed1d05121b235b50f872ce4eda2dbbe891
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-06-11 16:56:14 -0300

    DataBase has been actualized

--- Data Base/Dump_dbbiblio_20200611.sql
+++ Data Base/Dump_dbbiblio_20200611.sql
@@ -48,6 +48,38 @@ LOCK TABLES `tb_book` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tb_client`
+--
+
+DROP TABLE IF EXISTS `tb_client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_client` (
+  `client_name` varchar(150) NOT NULL,
+  `client_CEP` varchar(30) DEFAULT NULL,
+  `client_address` varchar(150) NOT NULL,
+  `client_phone` varchar(25) NOT NULL,
+  `cliente_login` varchar(50) NOT NULL,
+  `client_password` varchar(50) NOT NULL,
+  `client_image_perfil` varbinary(55530) DEFAULT NULL,
+  `client_email` tinytext,
+  `id_client` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_client`),
+  UNIQUE KEY `client_password` (`client_password`),
+  UNIQUE KEY `id_client` (`id_client`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_client`
+--
+
+LOCK TABLES `tb_client` WRITE;
+/*!40000 ALTER TABLE `tb_client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tb_rent`
 --
 
@@ -58,16 +90,17 @@ CREATE TABLE `tb_rent` (
   `id_rent` int(11) NOT NULL AUTO_INCREMENT,
   `rented_date` date NOT NULL,
   `id_of_book` int(11) NOT NULL,
-  `id_of_client` int(11) NOT NULL,
+  `id_of_student` int(11) NOT NULL,
   `return_date` date NOT NULL,
   `returned` tinyint(1) NOT NULL,
   `penality` double DEFAULT NULL,
   `observation` text,
+  `id_of_client` int(11) NOT NULL,
   PRIMARY KEY (`id_rent`),
   KEY `idlivro` (`id_of_book`),
-  KEY `idcli` (`id_of_client`),
+  KEY `idcli` (`id_of_student`),
   CONSTRAINT `tb_rent_ibfk_1` FOREIGN KEY (`id_of_book`) REFERENCES `tb_book` (`id_book`),
-  CONSTRAINT `tb_rent_ibfk_2` FOREIGN KEY (`id_of_client`) REFERENCES `tb_student` (`id_client`)
+  CONSTRAINT `tb_rent_ibfk_2` FOREIGN KEY (`id_of_student`) REFERENCES `tb_student` (`id_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -168,4 +201,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-11 15:46:48
+-- Dump completed on 2020-06-11 16:54:46
